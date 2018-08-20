21cb37a2d97d9b051cc7892bf8890f4dfe5b2fb8
feene <39316086+feene@users.noreply.github.com>
2018-08-19 20:53:52 -0600

    Code Clean up v1.
    Cleaned up code with some comments and formatting.

--- GarageDoorSensor.ino/GarageDoorSensor.ino.ino
+++ GarageDoorSensor.ino/GarageDoorSensor.ino.ino
@@ -1,34 +1,56 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+
+ */
+
+
 // Enable debug prints
 //#define MY_DEBUG
 
 // Enable and select radio type attached
 //#define MY_RADIO_NRF24
 //#define MY_RADIO_NRF5_ESB
-#define MY_RADIO_RFM69
 //#define MY_RADIO_RFM95
 
+//Includes some additional RFM69 Definces that are needed for Mysensors 2.2 and 2.3
+#define MY_RADIO_RFM69
 #define MY_RFM69_FREQUENCY RFM69_433MHZ
 #define MY_IS_RFM69HW
 
 #include <MySensors.h>
 
-#define SKETCH_NAME "Binary Sensor"
+#define SKETCH_NAME "Binary Sensor - Garage Door Tilt Sensor"
 #define SKETCH_MAJOR_VER "1"
 #define SKETCH_MINOR_VER "0"
 
+//Child ID for this sensor
 #define PRIMARY_CHILD_ID 3
 
+//Using Pin 3 because of Inturrupt to wake up the node
 #define PRIMARY_BUTTON_PIN 3   // Arduino Digital I/O pin for button/reed switch
 
-
+//Analog Pin to detect Battery Voltage
 int BATTERY_SENSE_PIN = A0;  // select the input pin for the battery sense point
 int oldBatteryPcnt = 0;
+int SLEEP_TIME = 7200000;
 
-// Change to V_LIGHT if you use S_LIGHT in presentation below
 MyMessage msg(PRIMARY_CHILD_ID, V_TRIPPED);
-
-
-
 
 
 void setup()
@@ -41,13 +63,6 @@ void setup()
   analogReference(INTERNAL);
 #endif
 }
-
-
-
-
-
-
-
 
 
 void presentation()
@@ -64,16 +79,18 @@ void presentation()
 // Loop will iterate on changes on the BUTTON_PINs
 void loop()
 {
+
+// Begin Battery Level Reporting
   int sensorValue = analogRead(BATTERY_SENSE_PIN);
   int batteryPcnt = sensorValue / 10;
   Serial.println(sensorValue);
-  
+
     if (oldBatteryPcnt != batteryPcnt) {
     // Power up radio after sleep
     sendBatteryLevel(batteryPcnt);
     oldBatteryPcnt = batteryPcnt;
   }
-  #ifdef MY_DEBUG
+#ifdef MY_DEBUG
   float batteryV  = sensorValue * 0.003363075;
   Serial.print("Battery Voltage: ");
   Serial.print(batteryV);
@@ -84,13 +101,10 @@ void loop()
   Serial.println(" %");
 #endif
 
+// End Battery Level Reporting
 
 
-
-
-
-
-  
+// Being Tilt Sensor Reporting
   uint8_t value;
   static uint8_t sentValue=2;
 
@@ -105,8 +119,6 @@ void loop()
     sentValue = value;
   }
 
-  // Sleep until something happens with the sensor
-  sleep(PRIMARY_BUTTON_PIN-2, CHANGE, 7200000);
+  // Sleep until something happens with the sensor.  Change on D3
+  sleep(PRIMARY_BUTTON_PIN-2, CHANGE, SLEEP_TIME);
 }
-
-
